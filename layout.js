676ee65d05f43588b36b75a0c52c28f88671215b
meathorse <meathorse@hotmail.com>
2026-06-07 17:09:42 +1200

    Add files via upload

--- layout.js
+++ layout.js
@@ -8,7 +8,7 @@ if (window.location.hostname === 'localhost' || window.location.hostname === '12
     basePath = '/thecan/'; 
 } else {
     // Web path
-    basePath = 'https://meathorse.github.io/thecan/';
+    basePath = '/thecan/';
 }
         // // Inject <base> ONLY on GitHub Pages
         // if (location.hostname === "https://meathorse.github.io") {
