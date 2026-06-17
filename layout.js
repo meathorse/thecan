2382effd9991af2356b8c8f4d8671758db3a6f0a
meathorse <meathorse@hotmail.com>
2026-06-18 00:06:09 +1200

    Add files via upload

--- layout.js
+++ layout.js
@@ -6,8 +6,17 @@ class globalheader extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
+<div class="brand">
+    <div class="brand-logo">
+        <a href="index.html"><img src="can.png"></a>
+    </div>
+
+    <div class="brand-text">
         <h1>TheCan</h1>
-        <h2>The empty can rattles the most</h2>`;
+        <h2>The empty can rattles the most</h2>
+    </div>
+</div>`;
+            
     }
 }
 
@@ -23,14 +32,31 @@ class globalnav extends HTMLElement{
         
     </nav>
                 `
+    // Highlight active link
+        const links = this.querySelectorAll("a");
+        const current = window.location.pathname;
+        const currentPage = current.split("/").pop();
+
+        links.forEach(link => {
+            const linkPage = link.getAttribute("href").split("/").pop();
+            if (currentPage === linkPage) {
+                link.classList.add("active");
+            }
+        });
     }
 }
 
 class globalfooter extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
-        <h5>---  Author: TC  ---  © 2026  ---  <a href="mailto:meathorse@hotmail.com">Contact</a>  ---</h5>
-        `
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="html/tech_index.html">Technology</a>
+                <a href="html/linux_index.html">Linux</a>
+                <a href="html/sci_index.html">Science</a>
+                <a href="html/autos_index.html">Autos</a> 
+            </nav>
+            <h5>Author: TC <br> © 2026 <br> <a href="mailto:meathorse@hotmail.com">Contact</a></h5>`
     }
 }
 
